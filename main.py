802047a73bb6e36d009e9a93acd3ff317c9edd3e
Maria (Malu) Luiza <148253498+marialuiza15@users.noreply.github.com>
2026-06-01 03:00:27 +0000

    Atualizar main.py e leitura_INMET.py com leitura dos arquivos INMET

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from leituras.leitura_INMET import junta_clima
-# from leituras.leitura_IBGE import junta_ibge
-# from leituras.leitura_SUS import junta_saude
+from leituras.leitura_IBGE import junta_ibge
+from leituras.leitura_SUS import junta_saude
 
 df_clima = junta_clima()
-# df_ibge = junta_ibge()
-# df_saude = junta_saude()
+df_ibge = junta_ibge()
+df_saude = junta_saude()
 
